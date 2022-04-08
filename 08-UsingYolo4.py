088a3d68b4fa7d30b012b3ffd9f5fe9e90ec4f59
Miller.Huang <Miller.Huang@lumens.com.tw>
2022-04-08 17:21:09 +0800

    Add YoloV4 sample

---
+++ 08-UsingYolo4.py
@@ -0,0 +1,20 @@
+"""
+refer: https://wiki.loliot.net/docs/lang/python/libraries/yolov4/python-yolov4-about/
+tensorflow: 2.8.0
+opencv: 4.5.4
+"""
+import cv2
+from yolov4.tf import YOLOv4
+
+yolo = YOLOv4()
+yolo.config.parse_names("./YoloV4Dataset/coco.names")
+yolo.config.parse_cfg("./YoloV4Dataset/yolov4-tiny.cfg")
+
+yolo.make_model()
+yolo.load_weights("./YoloV4Dataset/yolov4-tiny.weights", weights_type="yolo")
+
+yolo.summary(summary_type="yolo")
+yolo.summary()
+
+yolo.inference(media_path="./img/2.jpg")
+
