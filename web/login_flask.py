76414042e526fc6d8155b979f1ad5bf697e60762
mychuang <hhjoy222@gmail.com>
2022-05-28 21:25:54 +0800

    Add testLogin

---
+++ web/login_flask.py
@@ -0,0 +1,21 @@
+import numpy as np
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
+from flask import render_template, request, redirect, url_for, session
+
+#Flask 類別 初始化時 傳入的 __name__ 參數，代表當前模組的名稱。是固定用法，以便讓 Flask 知道在哪裡尋找資源。
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def testLogin():
+    # 利用request取得使用者端傳來的方法為何
+    if request.method == 'POST':
+        #  利用request取得表單欄位值
+        return 'Hello ' + request.values['username']
+
+    #  非POST的時候就會回傳一個空白的模板
+    return render_template('testLogin.html')
+
+if __name__ == '__main__':
+    app.run()
