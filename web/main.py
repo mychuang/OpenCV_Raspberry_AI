3e6f39279d19297666c0557221d79e4854ff7ce5
mychuang <hhjoy222@gmail.com>
2022-05-28 20:53:43 +0800

    resize base.html

--- web/main.py
+++ web/main.py
@@ -14,9 +14,35 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login_html():
+    if request.method == 'POST':
+
     return render_template("login.html")
+
+"""
+# 登入頁面
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
+        curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        curl.execute("SELECT * FROM users WHERE email=%s", [email])
+        user = curl.fetchone()
+        curl.close()
+        if user == None:
+            return "沒有這個帳號"
+        if len(user) != 0:
+            if bcrypt.hashpw(password, user["password"].encode('utf-8')) == user["password"].encode('utf-8'):
+                session['name'] = user['name']
+                session['email'] = user['email']
+                return render_template("home.html")
+            else:
+                return "您的密碼錯誤"
+    else:
+        return render_template("login.html")
+"""
 
 @app.route("/baseHome")
 def home_html():
