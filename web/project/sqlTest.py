a858a801f3666d3d46d1ad2f89f268b4b56acdb1
Miller.Huang <Miller.Huang@lumens.com.tw>
2022-05-29 15:11:56 +0800

    update sqlTest

--- web/project/sqlTest.py
+++ web/project/sqlTest.py
@@ -1,11 +1,16 @@
 import pymysql as mysql
 
-db = mysql.connect(host="localhost", user="mychuang", passwd="vipoo202", db="mydatabase")
+db = mysql.connect(host="localhost", user="mychuang", passwd="vipoo202", db="db")
 
 cursor = db.cursor() #connect
-cursor.execute("SELECT * FROM users WHERE email=hhjoy222@gmail.com")
+cursor.execute("SELECT * FROM users WHERE name='Miller'")
 
 user = cursor.fetchone()
 cursor.close()
 
 print(user)
+print(len(user))
+print(user[0])
+print(user[1])
+print(user[2])
+print(user[3])
