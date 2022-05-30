07d3f0af39254e52187d4956ec41768e3177e6a2
Miller.Huang <Miller.Huang@lumens.com.tw>
2022-05-30 15:01:35 +0800

    Complete the framework of front-end

---
+++ web/location/index.js
@@ -0,0 +1,25 @@
+function showPosition(position) {
+    $("#lat").val(position.coords.latitude);
+    $("#lon").val(position.coords.longitude);
+
+    let date = new Date();
+    var m = document.getElementById("msg");
+
+    m.innerHTML = "日期: " + date.toISOString().split('T')[0];
+
+}
+
+//取得 經緯度
+function getLocation(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);//有拿到位置就呼叫 showPosition 函式
+    } else {
+        m.innerHTML = "您的瀏覽器不支援顯示地理位置 API ，請使用其它瀏覽器開啟這個網址";
+    }
+}
+
+$(document).ready(function(){
+    $("#btn").click(function(){
+        getLocation();
+    });
+});
