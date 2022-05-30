74ed1c9d3dde7cd360b73403bfac7ae2d35d6080
Miller.Huang <Miller.Huang@lumens.com.tw>
2022-05-30 16:40:26 +0800

    Complete the Ajax to Flask sample

---
+++ web/note06/js2flask.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import url_for
+from flask import redirect
+from flask import render_template
+from flask import request
+import json
+
+app = Flask(__name__)
+testInfo = {}
+
+@app.route('/')
+def home():
+    return render_template('js2flask.html')
+
+@app.route('/getRequest', methods=['GET', 'POST'])
+def test():
+    testInfo['name'] = 'mychuang'
+    testInfo['age'] = '30'
+    return json.dumps(testInfo)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
