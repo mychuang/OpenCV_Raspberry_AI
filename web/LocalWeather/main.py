ba2febe1d1968bfe8de822617b3bb9f5ad1a784c
Miller.Huang <Miller.Huang@lumens.com.tw>
2022-06-01 17:43:39 +0800

    Add sql flag when using in RaspberryPi

--- web/LocalWeather/main.py
+++ web/LocalWeather/main.py
@@ -2,6 +2,16 @@ from flask import Flask, url_for, redirect, render_template, request, session
 import json
 import WeatherData as wd
 from datetime import datetime
+import pymysql as mysql
+
+sqlFlag = False
+try:
+    db = mysql.connect(host="localhost", user="mychuang", passwd="vipoo202", db="db")
+    cursor = db.cursor()
+    print("sucess to connect to sql")
+    sqlFlag = True
+except:
+    print("cannot use sql")
 
 app = Flask(__name__)
 
@@ -14,31 +24,65 @@ def login():
 
 @app.route('/setRequest', methods=['GET', 'POST'])
 def setData():
-    session["pwd"] = request.form.get('pwd')
-    session["user"] = request.form.get('user')
-    session['lat'] = request.form.get('lat')
-    session['lon'] = request.form.get('lon')
-    session['date'] = request.form.get('date')
-    print(session['lat'])
-    print(session['lon'])
-    # get weather
-    Times = session['date'].split('-', 2)
-    try:
-        weatherObs = wd.obs.get(lat=float(session['lat']), lon=float(session['lon']),
-                        dtime=datetime(int(Times[0]), int(Times[1]), int(Times[2])))
-        print("get temp ", weatherObs['tx'])
-        session['ws'] = weatherObs['ws']
-        session['tx'] = weatherObs['tx']
-        session['pres'] = weatherObs['pres']
-        session['precp_hour'] = weatherObs['precp_hour']
-    except:
-        print("setData: error occures when get obs")
-        session['ws'] = -999.9
-        session['tx'] = -999.9
-        session['pres'] = -999.9
-        session['precp_hour'] = -999.9
+    global sqlFlag
 
-    return json.dumps({"msg": "success"})
+    if(sqlFlag):
+        # 確認使用者身分
+        sql = "SELECT * FROM users WHERE name=" + "'" + request.form.get('user') + "'"
+        cursor.execute(sql)
+        user = cursor.fetchone()
+        cursor.close()
+
+        if user == None:
+            print("Non user in database")
+            return render_template('login.html')
+        if len(user) > 0:
+            session["pwd"] = request.form.get('pwd')
+            session["user"] = request.form.get('user')
+            session['lat'] = request.form.get('lat')
+            session['lon'] = request.form.get('lon')
+            session['date'] = request.form.get('date')
+
+            # get weather
+            Times = session['date'].split('-', 2)
+            try:
+                weatherObs = wd.obs.get(lat=float(session['lat']), lon=float(session['lon']),
+                                        dtime=datetime(int(Times[0]), int(Times[1]), int(Times[2])))
+                session['ws'] = weatherObs['ws']
+                session['tx'] = weatherObs['tx']
+                session['pres'] = weatherObs['pres']
+                session['precp_hour'] = weatherObs['precp_hour']
+            except:
+                print("setData: error occures when get obs")
+                session['ws'] = -999.9
+                session['tx'] = -999.9
+                session['pres'] = -999.9
+                session['precp_hour'] = -999.9
+
+            return json.dumps({"msg": "success"})
+    else:
+        session["pwd"] = request.form.get('pwd')
+        session["user"] = request.form.get('user')
+        session['lat'] = request.form.get('lat')
+        session['lon'] = request.form.get('lon')
+        session['date'] = request.form.get('date')
+        # get weather
+        Times = session['date'].split('-', 2)
+        try:
+            weatherObs = wd.obs.get(lat=float(session['lat']), lon=float(session['lon']),
+                        dtime=datetime(int(Times[0]), int(Times[1]), int(Times[2])))
+            session['ws'] = weatherObs['ws']
+            session['tx'] = weatherObs['tx']
+            session['pres'] = weatherObs['pres']
+            session['precp_hour'] = weatherObs['precp_hour']
+        except:
+            print("setData: error occures when get obs")
+            session['ws'] = -999.9
+            session['tx'] = -999.9
+            session['pres'] = -999.9
+            session['precp_hour'] = -999.9
+
+        return json.dumps({"msg": "success"})
 
 @app.route('/home')
 def home():
@@ -67,14 +111,33 @@ def regist():
 
 @app.route("/setRegist", methods=['GET', 'POST'])
 def setRegist():
-    print(request.form.get('pwd'))
-    print(request.form.get('user'))
-    print(request.form.get('email'))
+    if (sqlFlag):
+        sql = "SELECT COUNT(id) FROM users"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        cursor.close()
+        id = result[0] + 1
 
-    print('insert sql')
+        var1 = "'" + str(id) + "',"
+        var2 = "'" + request.form.get('user') + "',"
+        var3 = "'" + request.form.get('email') + "',"
+        var4 = "'" + request.form.get('pwd') + "'"
+        sql = "INSERT INTO users VALUES (" + var1 + var2 + var3 + var4 + ");"
+        print(sql)
+        cursor.execute(sql)
+        db.commit()  # send sql
+
+    else:
+        print(request.form.get('pwd'))
+        print(request.form.get('user'))
+        print(request.form.get('email'))
+
     return json.dumps({"msg": "success"})
 
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = "#230dec61-fee8-4ef2-a791-36f9e680c9fc"
-    app.run()
+    if (sqlFlag):
+        app.run(host="0.0.0.0", port=8888)
+    else:
+        app.run()
