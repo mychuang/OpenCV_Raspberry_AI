11428867f543ca0c5485c82501a67b19768aa789
Miller.Huang <Miller.Huang@lumens.com.tw>
2022-06-01 14:47:49 +0800

    Add regular method in JS code

--- web/LocalWeather/static/js/regist.js
+++ web/LocalWeather/static/js/regist.js
@@ -1,7 +1,52 @@
+function valid(){
+    // Get form references:
+	var name = $("#username").val();
+	var pwd = $("#password").val();
+	var email = $("#email").val();
+
+    // Flag variable:
+	var error = 0;
+
+	if (/^[A-Z \.\-']{2,20}$/i.test(name)) {
+		$("#pname").text("");
+	} else {
+		$("#pname").text("格式錯誤: /^[A-Z \.\-']{2,20}$/");
+		error++;
+	}
+
+    if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{8,16}$/i.test(pwd)) {
+		$("#ppwd").text("");
+	} else {
+		$("#ppwd").text("8-16個字符,至少1個大寫字母,1個小寫字母和1個數字");
+		error++;
+	}
+
+    if (/^[\w.-]+@[\w.-]+\.[A-Za-z]{2,6}$/.test(email)) {
+		$("#pemail").text("");
+	} else {
+		$("#pemail").text("格式錯誤: /^[\w.-]+@[\w.-]+\.[A-Za-z]{2,6}$/");
+		error++;
+	}
+
+    if(error>0){
+        console.log("error counts: ", error);
+    }else{
+        console.log("Ajax");
+    }
+    error = 0;
+}
+
 $(document).ready(function(){
+    
+    checkBox = document.getElementById('checker').addEventListener('click', event => {
+        if(event.target.checked) {
+            $("#btn").removeAttr("disabled");
+        }else{
+            $("#btn").attr("disabled", true);
+        }
+    });
+
     $("#btn").click(function(){
-        console.log($("#username").val());
-        console.log($("#password").val());
-        console.log($("#email").val());
-    })
-});
+        valid();
+    });
+})
